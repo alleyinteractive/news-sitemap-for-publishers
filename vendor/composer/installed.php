67a003692ce7c1a590bf42058f8ae3dafc6444b2
mboynes <mboynes@users.noreply.github.com>
2025-12-30 02:24:25 +0000

    Built changes from main f97b4749d07d5fd01bfad1043a433d9629bc0b56

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alleyinteractive/wp-news-sitemap',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f97b4749d07d5fd01bfad1043a433d9629bc0b56',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'alleyinteractive/wp-news-sitemap' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f97b4749d07d5fd01bfad1043a433d9629bc0b56',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
