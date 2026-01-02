e6b254f0ab491d4613cc2c688f3eb9c566af3f3d
mboynes <mboynes@users.noreply.github.com>
2026-01-02 18:22:56 +0000

    Built changes for v1.2.1 from develop 90936fa77be74d8110ae987bc21adb06f0577591

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alleyinteractive/news-sitemap-for-publishers',
+        'pretty_version' => '1.2.1',
+        'version' => '1.2.1.0',
+        'reference' => null,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'alleyinteractive/news-sitemap-for-publishers' => array(
+            'pretty_version' => '1.2.1',
+            'version' => '1.2.1.0',
+            'reference' => null,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
